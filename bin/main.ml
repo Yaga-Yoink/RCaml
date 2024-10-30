18eb95d650b0dd051564ca1a6515ec2f89d100c6
Edward Handjaja <eeh83@cornell.edu>
2024-10-29 21:12:58 -0400

    Implement Main.ml - Input and output of txt files

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,21 @@
-let () = print_endline "Hello, World!"
+open Batteries
+
+let fileProcessor (fileName : string) : string list list =
+  BatFile.lines_of fileName
+  |> BatEnum.map (fun line ->
+         line |> String.split_on_char ' '
+         |> List.filter (fun word -> word <> ""))
+  |> BatList.of_enum
+
+let printToOutput (lst : string list) (output_file : string) : unit =
+  try
+    let output_channel = open_out output_file in
+    List.iter (fun line -> Printf.fprintf output_channel "%s\n" line) lst;
+    close_out output_channel;
+    Printf.printf "Successfully wrote to file: %s\n" output_file
+  with e -> Printf.printf "Error writing to file: %s\n" (Printexc.to_string e)
+
+let () =
+  let sample_output = [ "7"; "c(1,13)" ] in
+  let outputFile = "data/sample_corrected.txt" in
+  printToOutput sample_output outputFile
