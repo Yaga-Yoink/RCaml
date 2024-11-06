9caeef3da4e680f361dcba50320014dc7a1a4aa5
Yaga-Yoink <rfujii3012@gmail.com>
2024-11-06 01:58:15 -0500

    Added documentation for parse function

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,7 @@
 open Ast
 
+(** [parse s] is the interpreted abstract syntax tree from applying the lexing
+    and parser grammar rules on [s]. *)
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
