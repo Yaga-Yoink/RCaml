40f28cfaa287d5b9a6005330f08f8e885a01ff2f
ak2498 <ak2498@cornell.edu>
2024-12-11 14:44:32 -0500

    Include help command

--- lib/processLines.mli
+++
@@ -1,3 +0,0 @@
-(* val process_input : string list list -> string list (** [process_input lst]
-   processes a list of lines [lst], using the global variable hashmap for
-   assignments and returns a list of results. *) *)
